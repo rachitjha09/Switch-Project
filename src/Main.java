33777e099083474b1aa1e0e442478c7d429b5ed0
rachitjha09 <rachitjha09@gmail.com>
2024-10-02 13:02:10 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.sql.SQLOutput;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int x=1;
+        switch(x){
+            case 1:
+                System.out.println("a");
+                break;
+            case 2:
+                System.out.println("b");
+                break;
+            case 3:
+                System.out.println("c");
+                break;
+            case 4:
+                System.out.println("d");
+                break;
+            default:
+                System.out.println("e");
+                break;
+        }
+    }
+}
